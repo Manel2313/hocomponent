7c43f3ff11949302c9ebc662ac0cc109b055ddac
Manel2313 <djemal.manel@gmail.com>
2019-05-23 14:01:28 +0100

    Updates

---
+++ precache-manifest.a0bd6abbf360c59aaafbdca07995a590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8feea68dc4f2a23e437aef6956890d3",
+    "url": "/hocomponent/index.html"
+  },
+  {
+    "revision": "b38482b8bd7b33f8afd3",
+    "url": "/hocomponent/static/css/main.4565f4c3.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/hocomponent/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b38482b8bd7b33f8afd3",
+    "url": "/hocomponent/static/js/main.520e2765.chunk.js"
+  },
+  {
+    "revision": "3cf3c60674dfc0379f61",
+    "url": "/hocomponent/static/js/runtime~main.f146c2d3.js"
+  }
+]);
